b565111869c0299218a82a391b3fa72dd0776aef
ItsMukundKumar <shahmukund060@gmail.com>
2026-07-23 21:26:27 +0530

    test-5

--- scripts/review.py
+++ scripts/review.py
@@ -14,7 +14,7 @@ llm = ChatGoogleGenerativeAI(model="gemini-3.1-flash-lite")
 def main():
     diff = getDiff()
     
-    prompt = f"Review the following code changes and provide feedback:\n\n{diff}"
+    prompt = f"Review the following code changes and provide feedback:\n\n{diff}\n\nMandatory: Provie output in HTML formate that can use to sent in mail"
     response = llm.invoke(prompt)
     print("Code Review Feedback: ")
     print(response.text)
