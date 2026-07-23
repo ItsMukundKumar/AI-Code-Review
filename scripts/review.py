520b308b9bae02c7d176ea6bda7af604c1969138
ItsMukundKumar <shahmukund060@gmail.com>
2026-07-23 20:50:43 +0530

    added AI

--- scripts/review.py
+++ scripts/review.py
@@ -1,8 +1,17 @@
 import subprocess
+import os
+from langchain_google_genai.chat_models import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def getDiff():
     return subprocess.check_output(['git', 'show'], text=True)
 
 
-print(getDiff())
+llm = ChatGoogleGenerativeAI(model="gemini-3.1-flash-lite")
+
+output = llm.invoke('Hello!')
+
+
+print(output.content)
